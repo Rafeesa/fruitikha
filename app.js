ce867da7958b26fb464873ef4bc11721f9430ca6
rafeesa <rafeesapc00@gmail.com>
2024-12-19 04:14:18 +0530

    port number changed

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
