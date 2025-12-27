33026dc55ab47c8909a7e21af25b0e02bef21a26
rafeesa <rafeesapc00@gmail.com>
2025-12-27 12:39:12 -0300

    Fix Google OAuth behind nginx proxy

--- config/passport.js
+++ config/passport.js
@@ -12,8 +12,9 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
+       proxy: true,
 
-     // callbackURL: '/auth/google/callback',
+    
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
